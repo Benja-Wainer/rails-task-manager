8094d222e99e32f0bc880c4e3b9debbd258dbea6
Benjamin Wainer <benjawainer@gmail.com>
2023-07-01 14:06:29 +0900

    User can modify tasks, also creating one now redirects to the correct page.

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,13 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   # Read all
-  get 'tasks', to: 'tasks#index'
+  get 'tasks', to: 'tasks#index', as: :tasks
   # Create
   get 'tasks/new', to: 'tasks#new', as: :new_task
   post 'tasks', to: 'tasks#create'
   # Read one
   get 'tasks/:id', to: 'tasks#show', as: :task
+  # Update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
 end
