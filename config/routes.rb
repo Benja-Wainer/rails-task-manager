f3cf93a10d34d0ce1ba3846de66fa018a8afdb89
Benjamin Wainer <benjawainer@gmail.com>
2023-07-01 12:54:33 +0900

    Index page and show page working with links.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Read all
+  get 'tasks', to: 'tasks#index', as: 'tasks'
+  # Read one
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 end
