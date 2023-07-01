9d343cc6b39d1578d54b6c3ebe76f881b2b66e74
Benjamin Wainer <benjawainer@gmail.com>
2023-07-01 13:30:38 +0900

    User is able to create new tasks, with related links added to the pages.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   # Read all
-  get 'tasks', to: 'tasks#index', as: 'tasks'
+  get 'tasks', to: 'tasks#index'
+  # Create
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
   # Read one
-  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
